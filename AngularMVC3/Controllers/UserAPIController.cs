9cae277a6c0764febfba9631eaba489df85fba8e
apexbugfinder <apexbugfinder@gmail.com>
2018-02-23 03:32:04 -0800

    Added comments to the UserAPIController

--- AngularMVC3/Controllers/UserAPIController.cs
+++ AngularMVC3/Controllers/UserAPIController.cs
@@ -4,36 +4,43 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AngularMVC3.DBContext;
+using System.Data.Entity;
+
 
 namespace AngularMVC3.Controllers
 {
-    public class UserAPIController : ApiController
+    public class UserAPIController : BaseAPIController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        // Reads all Users from BaseAPIController
+        // Returns them in a HTTP Response Message converted to JSON string
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value1", "value2" };
+            return ToJson(UserDB.TblUsers.AsEnumerable());
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
+        // Creates new user information and adds them to the database
+        // Returns a 1 for successfully saved
+        public HttpResponseMessage Post([FromBody]TblUser value)
         {
-            return "value";
+            UserDB.TblUsers.Add(value);
+            return ToJson(UserDB.SaveChanges());
         }
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
+        // Takes the existing user's id and updated information and updates the database
+        // Returns a 1 for successfully updated
+        public HttpResponseMessage Put(int id, [FromBody]TblUser value)
         {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        // Uses existing user id and deletes user from database
+        // Returns a 1 if entry is successfully deleted
+        public HttpResponseMessage Delete(int id)
         {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            UserDB.TblUsers.Remove(UserDB.TblUsers.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
         }
     }
 }
